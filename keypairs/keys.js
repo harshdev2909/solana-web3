e968dedf83a90b978d1b48f41e16eb69f730efb0
harsh sharma <165451996+harshdev2909@users.noreply.github.com>
2025-03-31 11:14:44 +0530

    changes

---
+++ keypairs/keys.js
@@ -0,0 +1,21 @@
+import { Keypair } from "@solana/web3.js";
+import nacl from "tweetnacl";
+
+const KeyPair = Keypair.generate()
+
+const publickey = KeyPair.publicKey.toString()
+const secretKey = KeyPair.secretKey
+
+console.log("Public Key:", publickey);
+console.log("Private Key (Secret Key):", secretKey);
+
+// Convert the message "hello world" to a Uint8Array
+const message = new TextEncoder().encode("hello world");
+const signature = nacl.sign.detached(message, secretKey);
+const result = nacl.sign.detached.verify(
+    message,
+    signature,
+    KeyPair.publicKey.toBytes(),
+  );
+  
+  console.log(result);
---
+++ keypairs/readme.md
@@ -0,0 +1,15 @@
+# Solana Keypair Signature Example
+
+This example demonstrates how to generate a Solana keypair, sign a message using the `tweetnacl` library, and verify the signature. It uses the `@solana/web3.js` library for keypair generation and `tweetnacl` for signing and verifying the message.
+
+## Prerequisites
+
+Before running this example, make sure you have the following dependencies installed:
+
+- `@solana/web3.js`
+- `tweetnacl`
+
+You can install these dependencies using npm:
+
+```bash
+npm install @solana/web3.js tweetnacl
